f42f515a8aaef543e2652d8771db40212c2ccaab
Nishad Guha <nishadguha@Nishads-MacBook-Pro.local>
2021-10-15 22:56:37 +0200

    fix(HashRouter)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './App.scss';
 import SuccessPage from './components/SuccessPage';
 import Survey from './components/Survey';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
